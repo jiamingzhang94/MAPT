a39be065bb47affbc22731f4a5bc5d52f7255e33
jmzhang <z490938304@163.com>
2025-03-03 08:23:37 +0800

    first

--- scripts/main.sh
+++ scripts/main.sh
@@ -7,27 +7,37 @@ DATA="/home/dycpu6_8tssd1/jmzhang/datasets/"
 TRAINER=MaPLe
 
 #DATASETS=(
-#    "oxford_pets"
-#    "oxford_flowers"
-#    "fgvc_aircraft"
-#    "dtd"
-#    "eurosat"
-#    "stanford_cars"
-#    "food101"
-#    "sun397"
-#    "caltech101"
-#    "ucf101"
-#    "imagenet"
-#    # "imagenet_sketch"
-#    # "imagenetv2"
-#    # "imagenet_a"
-#    # "imagenet_r"
+#    "oxford_pets"
+#    "oxford_flowers"
+#    "fgvc_aircraft"
+#    "dtd"
+#    "eurosat"
+#    "stanford_cars"
+#    "food101"
+#    "sun397"
+#    "caltech101"
+#    "ucf101"
+#    "imagenet"
+#    # "imagenet_sketch"
+#    # "imagenetv2"
+#    # "imagenet_a"
+#    # "imagenet_r"
 #)
 DATASET=oxford_pets
 SEED=1
 CFG=vit_b16_c2_ep5_batch4_2ctx
 
-DIR=checkpoints/${DATASET}/${TRAINER}/${CFG}/feature
+# Set FEATURE_MODE to either "true" or "false" to control --feature argument
+FEATURE_MODE="true" # Change to "false" if you want --feature False
+
+if [ "$FEATURE_MODE" = "true" ]; then
+  DIR=checkpoints/${DATASET}/${TRAINER}/${CFG}/feature
+  FEATURE_ARG="--feature True"
+else
+  DIR=checkpoints/${DATASET}/${TRAINER}/${CFG}/ce
+  FEATURE_ARG="--feature False"
+fi
+
 python train.py \
 --root ${DATA} \
 --seed ${SEED} \
@@ -36,29 +46,4 @@ python train.py \
 --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
 --output-dir ${DIR} \
 --adv-train True \
---feature True
-
-
-#if [ -d "$DIR" ]; then
-#    echo "Results are available in ${DIR}. Resuming..."
-#    python train.py \
-#    --root ${DATA} \
-#    --seed ${SEED} \
-#    --trainer ${TRAINER} \
-#    --dataset-config-file configs/datasets/${DATASET}.yaml \
-#    --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
-#    --output-dir ${DIR} \
-#    DATASET.NUM_SHOTS ${SHOTS} \
-#    DATASET.SUBSAMPLE_CLASSES base
-#else
-#    echo "Run this job and save the output to ${DIR}"
-#    python train.py \
-#    --root ${DATA} \
-#    --seed ${SEED} \
-#    --trainer ${TRAINER} \
-#    --dataset-config-file configs/datasets/${DATASET}.yaml \
-#    --config-file configs/trainers/${TRAINER}/${CFG}.yaml \
-#    --output-dir ${DIR} \
-#    DATASET.NUM_SHOTS ${SHOTS} \
-#    DATASET.SUBSAMPLE_CLASSES base
-#fi
+${FEATURE_ARG}
